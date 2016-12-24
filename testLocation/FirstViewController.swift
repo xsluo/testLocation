f1149b95d0627ddab706d1095c33a8e4103f6ce9
isoluo <xsluo@126.com>
2016-12-24 22:07:14 +0800

    Initial Commit

---
+++ testLocation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  testLocation
+//
+//  Created by isoluo on 2016/12/24.
+//  Copyright © 2016年 isoluo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
